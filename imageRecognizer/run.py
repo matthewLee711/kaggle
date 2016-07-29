6e58d72d31801d21705efdec0c0c677f353485b3
Lee <MATTHEW_LEE2@homedepot.com>
2016-07-29 15:48:52 -0500

    Added 1D vectorize images

--- imageRecognizer/run.py
+++ imageRecognizer/run.py
@@ -57,6 +57,8 @@ Taking image and streching pixels into one column of pixel vector 3072 numbers
 f(x, W) = 10, 1 --- 3072 numbers going into w (3072 x 1)
 Wx = 10 x 3072
 +b (bias - independent weight, bias for cat centered data would choose over dog)
+Essentially, function f is assigning scores to each class. image is classified well,
+if score is high
 
 W(stacked classifiers)
 
@@ -69,7 +71,53 @@ INTERESTING - combining all images to create mdoel = blur image . Linear has thi
 if you use linear classifier, use a gray Scale. yellow car might be frog
 '''
 
+#linear part 2
+#Loss and reg
+'''
+trying to use scores that will have similar.
+minimize - find w that gives lowest loss (measues unhappiness)
 
+Multiclass svm loss (binary support vector class) - this loss score defines how correct
+you are. High number is bad, low is good. take the max of all scores - highest score,
+then add 1
+
+loss function, just squaring
+square loss vs squared hinge loss
+number of classes - 1
+dont want to inflate score so set to zero
+'''
+
+'''
+Weight Regularization
+measure the niceness of your weight. adding objectives to loss. You append this to your hyperparameter
+This will fight with your other weights, but it will improve test performance
+l1, l2, elastic net(l1 + l2), max norm regularization
+L2 Regularization (weight decay) is most common.
+Goal of regularization is to use as many x as possible. Helps to identify image
+so [.25,.25.25.25] is perfered over [1,0,0,0], weights diffused
+more evidence is being accumulated for evidence
+'''
+#alter regularization -- internesting
+
+
+'''
+softmax classifier
+scores - unormalized log probabilities of the classes
+1. get all scores (unormalized log properties) and exponentiate all of them
+2. returns unormalized probabilities
+3. normalize (divide)
+4. returns raw probabilities
+5. need to minimize log probability
+goal is lower your total loss. but not to zero
+'''
+
+'''
+Optimization:
+Attempting to get to low loss.
+Gradient Descent
+Compute the slope to go down hill
+
+'''
 
 
 
