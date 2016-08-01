2783b014b1546ed826090391d679f788ac666b6b
Lee <MATTHEW_LEE2@homedepot.com>
2016-08-01 08:32:22 -0500

    Added checklist for training and cNN

---
+++ imageRecognizer/cNN.py
@@ -0,0 +1,8 @@
+'''
+cNN
+Check list:
+-Naive forward pass
+-Convolutions
+-max pooling
+
+'''
---
+++ imageRecognizer/train.py
@@ -0,0 +1,14 @@
+'''
+Check list
+-gradient check
+    Numerical + analytical gradient (backward pass)
+-mini batch gradient descent
+    gradient
+-optimization
+-loss function
+-backward pass
+-step size/learning rate
+-back propagation 
+
+
+'''
