66339211a8d06273aa8af2dff974ffe9be4550cc
Lee <MATTHEW_LEE2@homedepot.com>
2016-08-12 11:22:05 -0500

    Updated cifar10 data extraction

--- imageRecognizer/data_utils.py
+++ imageRecognizer/data_utils.py
@@ -44,3 +44,21 @@ def get_CIFAR10_data(num_training=49000, num_validation=1000, numtest=1000):
     mask = range(num_test)
     X_test = X_test[mask]
     y_test = y_test[mask]
+
+    # Normalize the data: subtract the mean image
+    mean_image = np.mean(X_train, axis=0)
+    X_train -= mean_image
+    X_val -= mean_image
+    X_test -= mean_image
+
+    # Transpose so that channels come first
+    X_train = X_train.transpose(0, 3, 1, 2).copy()
+    X_val = X_val.transpose(0, 3, 1, 2).copy()
+    X_test = X_test.transpose(0, 3, 1, 2).copy()
+
+    # Package data into a dictionary
+    return {
+      'X_train': X_train, 'y_train': y_train,
+      'X_val': X_val, 'y_val': y_val,
+      'X_test': X_test, 'y_test': y_test,
+    }
